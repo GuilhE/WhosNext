6f9ae33a1a37a65de3fbe2990d16beeb70098fee
GuilhE <gui.delgado@hotmail.com>
2024-12-05 12:18:18 +0000

    Updates KMP-NativeCoroutines and KMP-ObservableViewModel

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -39,7 +39,7 @@ kotlin {
             api(libs.kmp.koin.core)
             api(libs.kmp.kermit)
             //https://github.com/Kotlin/kotlinx-atomicfu/issues/469#issuecomment-2326868412
-            implementation("org.jetbrains.kotlinx:atomicfu:0.26.0")
+            implementation("org.jetbrains.kotlinx:atomicfu:0.26.1")
         }
         androidMain.dependencies { implementation(libs.kmp.koin.android) }
         wasmJsMain.dependencies { implementation(libs.kotlinx.browser) }
